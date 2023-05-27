cb2f61b53042f0d8208b1fa14c6b0e31ab5c4919
anas2001abdullah <anasabdullah388@hotmail.com>
2023-05-27 19:38:30 +0100

    login deploy

---
+++ login.js
@@ -0,0 +1,17 @@
+document.querySelector(".show-password-icon").addEventListener("click", function(event){
+      
+    var password = document.querySelector(".password");
+    
+    if (password.type === "password") {
+       password.type = "text";
+       password.style.paddingLeft = "45px";
+       password.style.textAlign = "left";
+       this.classList.remove("fa-eye");
+       this.classList.add("fa-eye-slash");
+       
+    } else {
+      password.type = "password";
+      this.classList.remove("fa-eye-slash");
+      this.classList.add("fa-eye");
+    }
+})
